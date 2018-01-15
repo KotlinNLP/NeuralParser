d3a12c1e2eaab19dc37b347e0f1d461aadcfcbf1
KotlinNLP Authors <github@kotlinnlp.com>
2018-01-15 17:42:35 +0100

    Add BiRNNAttentionArcStandardParser

---
+++ src/main/kotlin/com/kotlinnlp/neuralparser/parsers/arcstandard/attention/ArcStandardAttentionFeaturesExtractor.kt
@@ -0,0 +1,91 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.neuralparser.parsers.arcstandard.attention
+
+import com.kotlinnlp.dependencytree.Deprel
+import com.kotlinnlp.dependencytree.POSTag
+import com.kotlinnlp.neuralparser.templates.featuresextractor.AttentionFeaturesExtractor
+import com.kotlinnlp.neuralparser.utils.actionsembeddings.ActionsVectorsMap
+import com.kotlinnlp.neuralparser.utils.actionsembeddings.ActionsVectorsOptimizer
+import com.kotlinnlp.neuralparser.templates.inputcontexts.TokensAmbiguousPOSContext
+import com.kotlinnlp.simplednn.core.neuralnetwork.NetworkParameters
+import com.kotlinnlp.simplednn.core.optimizer.ParamsOptimizer
+import com.kotlinnlp.simplednn.deeplearning.attentionnetwork.AttentionNetworkParameters
+import com.kotlinnlp.simplednn.utils.DictionarySet
+import com.kotlinnlp.syntaxdecoder.syntax.DependencyRelation
+import com.kotlinnlp.syntaxdecoder.transitionsystem.Transition
+import com.kotlinnlp.syntaxdecoder.transitionsystem.models.arcstandard.ArcStandardTransition
+import com.kotlinnlp.syntaxdecoder.transitionsystem.models.arcstandard.transitions.ArcLeft
+import com.kotlinnlp.syntaxdecoder.transitionsystem.models.arcstandard.transitions.ArcRight
+import com.kotlinnlp.syntaxdecoder.transitionsystem.models.arcstandard.transitions.Root
+import com.kotlinnlp.syntaxdecoder.transitionsystem.models.arcstandard.transitions.Shift
+import com.kotlinnlp.syntaxdecoder.transitionsystem.state.templates.StackBufferState
+
+/**
+ * The [AttentionFeaturesExtractor] for the ArcStandard transition system.
+ *
+ * @param actionsVectors the encoding vectors of the actions
+ * @param actionsVectorsOptimizer the optimizer of the [actionsVectors]
+ * @param actionDecoderOptimizer the optimizer of the RNN used to decode the action features
+ * @param actionAttentionNetworkOptimizer the optimizer of the attention network used to decode the action features
+ * @param featuresEncodingSize the size of the features encoding
+ * @param posTags the dictionary set of POS tags
+ * @param deprelTags the dictionary set of deprels
+ */
+class ArcStandardAttentionFeaturesExtractor(
+  actionsVectors: ActionsVectorsMap,
+  actionsVectorsOptimizer: ActionsVectorsOptimizer,
+  actionDecoderOptimizer: ParamsOptimizer<NetworkParameters>,
+  actionAttentionNetworkOptimizer: ParamsOptimizer<AttentionNetworkParameters>,
+  featuresEncodingSize: Int,
+  private val deprelTags: DictionarySet<Deprel>,
+  private val posTags: DictionarySet<POSTag>
+) : AttentionFeaturesExtractor<
+  StackBufferState,
+  ArcStandardTransition,
+  TokensAmbiguousPOSContext>
+(
+  actionsVectors = actionsVectors,
+  actionsVectorsOptimizer = actionsVectorsOptimizer,
+  actionDecoderOptimizer = actionDecoderOptimizer,
+  actionAttentionNetworkOptimizer = actionAttentionNetworkOptimizer,
+  featuresEncodingSize = featuresEncodingSize
+) {
+
+  /**
+   * The actions embeddings map key of the transition of this action.
+   */
+  override val Transition<ArcStandardTransition, StackBufferState>.key: Int
+    get() = when (this) {
+      is Shift -> 0
+      is Root -> 1
+      is ArcLeft -> 2
+      is ArcRight -> 3
+      else -> throw RuntimeException("unknown transition")
+    }
+
+  /**
+   * The actions embeddings map key of the deprel of this action.
+   */
+  override val Transition<ArcStandardTransition, StackBufferState>.Action.deprelKey: Int
+    get() = when {
+      this.transition is Shift -> 0
+      this is DependencyRelation -> this@ArcStandardAttentionFeaturesExtractor.deprelTags.getId(this.deprel!!)!! + 1 // + shift offset
+      else -> throw RuntimeException("unknown action")
+    }
+
+  /**
+   * The actions embeddings map key of the POS tag of this action.
+   */
+  override val Transition<ArcStandardTransition, StackBufferState>.Action.posTagKey: Int
+    get() = when {
+      this.transition is Shift -> 0
+      this is DependencyRelation -> this@ArcStandardAttentionFeaturesExtractor.posTags.getId(this.posTag!!)!! + 1 // + shift offset
+      else -> throw RuntimeException("unknown action")
+    }
+}
---
+++ src/main/kotlin/com/kotlinnlp/neuralparser/parsers/arcstandard/attention/BiRNNAttentionArcStandardParserModel.kt
@@ -0,0 +1,85 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.neuralparser.parsers.arcstandard.attention
+
+import com.kotlinnlp.dependencytree.POSTag
+import com.kotlinnlp.neuralparser.language.CorpusDictionary
+import com.kotlinnlp.neuralparser.parsers.ScorerNetworkConfiguration
+import com.kotlinnlp.neuralparser.templates.parsers.birnn.ambiguouspos.attention.BiRNNAttentionParserModel
+import com.kotlinnlp.neuralparser.templates.parsers.birnn.ambiguouspos.attention.ActionNetworkConfiguration
+import com.kotlinnlp.simplednn.core.functionalities.activations.ActivationFunction
+import com.kotlinnlp.simplednn.core.layers.LayerType
+import com.kotlinnlp.simplednn.deeplearning.embeddings.EmbeddingsMap
+import com.kotlinnlp.syntaxdecoder.transitionsystem.state.scoreaccumulator.ScoreAccumulator
+
+/**
+ * The parser model for the ArcStandard parser based on the BiRNN with Transition+POS+Deprel joint scoring with
+ * attention features decoding.
+ *
+ * @property actionsScoresActivation the function used to activate the actions scores (can be null)
+ * @property scoreAccumulatorFactory a factory of score accumulators
+ * @property corpusDictionary a corpus dictionary
+ * @property nounDefaultPOSTag the 'Noun' POS tag used for title case unknown forms
+ * @property otherDefaultPOSTags the list of POS tags used for other unknown forms
+ * @property wordEmbeddingSize the size of each word embedding vector
+ * @property posEmbeddingSize the size of each POS embedding vector
+ * @property actionsEmbeddingsSize the size of each action embedding vector
+ * @property attentionSize the attention size of the action attention network
+ * @property preTrainedWordEmbeddings pre-trained word embeddings to add to the tokens encodings (default = null)
+ * @param biRNNConnectionType the recurrent connection type of the BiRNN used to encode tokens
+ * @param biRNNHiddenActivation the hidden activation function of the BiRNN used to encode tokens
+ * @param biRNNLayers number of stacked BiRNNs
+ * @param actionNetworkConfig the configuration of the memory network used to decode the actions scorer features
+ * @param scorerNetworksConfig the configuration of the scorer networks
+ */
+class BiRNNAttentionArcStandardParserModel(
+  actionsScoresActivation: ActivationFunction?,
+  scoreAccumulatorFactory: ScoreAccumulator.Factory,
+  corpusDictionary: CorpusDictionary,
+  nounDefaultPOSTag: POSTag,
+  otherDefaultPOSTags: List<POSTag>,
+  wordEmbeddingSize: Int,
+  posEmbeddingSize: Int,
+  actionsEmbeddingsSize: Int,
+  attentionSize: Int,
+  preTrainedWordEmbeddings: EmbeddingsMap<String>? = null,
+  biRNNConnectionType: LayerType.Connection,
+  biRNNHiddenActivation: ActivationFunction?,
+  biRNNLayers: Int,
+  actionNetworkConfig: ActionNetworkConfiguration,
+  scorerNetworksConfig: ScorerNetworkConfiguration
+) : BiRNNAttentionParserModel(
+  actionsScoresActivation = actionsScoresActivation,
+  scoreAccumulatorFactory = scoreAccumulatorFactory,
+  corpusDictionary = corpusDictionary,
+  nounDefaultPOSTag = nounDefaultPOSTag,
+  otherDefaultPOSTags = otherDefaultPOSTags,
+  wordEmbeddingSize = wordEmbeddingSize,
+  posEmbeddingSize = posEmbeddingSize,
+  actionsEmbeddingsSize = actionsEmbeddingsSize,
+  attentionSize = attentionSize,
+  preTrainedWordEmbeddings = preTrainedWordEmbeddings,
+  biRNNConnectionType = biRNNConnectionType,
+  biRNNHiddenActivation = biRNNHiddenActivation,
+  biRNNLayers = biRNNLayers,
+  actionNetworkConfig = actionNetworkConfig,
+  scorerNetworksConfig = scorerNetworksConfig,
+  numberOfTransitions = 4, // Shift, Wait, Root, ArcLeft, ArcRight
+  numberOfTransitionsVectors = 4 // Shift and Wait considered the same
+) {
+
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed from Serializable).
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+  }
+}
+
