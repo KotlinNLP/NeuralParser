972a1a3a80f71e200ed92a156d16fc3a71df675c
KotlinNLP Authors <github@kotlinnlp.com>
2018-06-15 15:28:23 +0200

    Delete functions not used

--- src/main/kotlin/com/kotlinnlp/neuralparser/utils/Extensions.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/utils/Extensions.kt
@@ -13,22 +13,6 @@ import com.kotlinnlp.neuralparser.language.Sentence
 import java.io.File
 
 /**
- * @index  index gets the nth element
- *         -index gets the nth-to-last element
- * @return the element at the given index or null if the index is not in the array range
- */
-fun <E>ArrayList<E>.getItemOrNull(index: Int): E? {
-  val accessIndex = if (index < 0) this.size + index else index
-  return if (accessIndex in 0..this.lastIndex) this[accessIndex] else null
-}
-
-/**
- * Returns an element at the given [index] or the result of calling the [defaultValue] function if the [index] is out of bounds of this list.
- */
-fun List<Int>.getOrElse(index: Int?, defaultValue: Int): Int =
-  if (index != null && index >= 0 && index <= lastIndex) get(index) else defaultValue
-
-/**
  * Populate an array of sentences from a tree-bank.
  *
  * @param filePath the file path of the tree-bank in CoNLL format
