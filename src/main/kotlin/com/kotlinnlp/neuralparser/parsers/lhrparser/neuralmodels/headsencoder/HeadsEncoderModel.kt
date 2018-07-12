65b70c082d87889fd3dcb187efc93a227e70c707
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-12 02:40:56 +0200

    Fix import

--- src/main/kotlin/com/kotlinnlp/neuralparser/parsers/lhrparser/neuralmodels/headsencoder/HeadsEncoderModel.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/parsers/lhrparser/neuralmodels/headsencoder/HeadsEncoderModel.kt
@@ -12,7 +12,7 @@ import com.kotlinnlp.simplednn.core.functionalities.initializers.GlorotInitializ
 import com.kotlinnlp.simplednn.core.functionalities.initializers.Initializer
 import com.kotlinnlp.simplednn.core.layers.LayerType
 import com.kotlinnlp.simplednn.deeplearning.birnn.BiRNN
-import com.kotlinnlp.simplednn.deeplearning.birnn.mergeconfig.ConcatFeedforwardMerge
+import com.kotlinnlp.simplednn.core.layers.models.merge.mergeconfig.ConcatFeedforwardMerge
 import java.io.Serializable
 
 /**
