f5de92cbf2ea52f02e78484d32b4a7b2f1f52b40
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-12 02:48:59 +0200

    Add the 'lssEncoder' as private class property and remove unnecessary builders

--- src/main/kotlin/com/kotlinnlp/neuralparser/parsers/lhrparser/LHRParser.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/parsers/lhrparser/LHRParser.kt
@@ -10,13 +10,10 @@ package com.kotlinnlp.neuralparser.parsers.lhrparser
 import com.kotlinnlp.neuralparser.parsers.lhrparser.decoders.CosineDecoder
 import com.kotlinnlp.neuralparser.parsers.lhrparser.utils.ArcScores
 import com.kotlinnlp.dependencytree.DependencyTree
-import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.linguisticdescription.sentence.token.properties.Position
 import com.kotlinnlp.neuralparser.parsers.lhrparser.neuralmodels.contextencoder.ContextEncoder
-import com.kotlinnlp.neuralparser.parsers.lhrparser.neuralmodels.contextencoder.ContextEncoderBuilder
 import com.kotlinnlp.neuralparser.parsers.lhrparser.neuralmodels.headsencoder.HeadsEncoder
-import com.kotlinnlp.neuralparser.parsers.lhrparser.neuralmodels.headsencoder.HeadsEncoderBuilder
 import com.kotlinnlp.neuralparser.parsers.lhrparser.neuralmodels.labeler.DeprelLabeler
-import com.kotlinnlp.neuralparser.parsers.lhrparser.neuralmodels.labeler.DeprelLabelerBuilder
 import com.kotlinnlp.neuralparser.parsers.lhrparser.utils.ArcScores.Companion.rootId
 import com.kotlinnlp.neuralparser.parsers.lhrparser.utils.CyclesFixer
 import com.kotlinnlp.neuralparser.NeuralParser
@@ -34,30 +31,19 @@ import com.kotlinnlp.tokensencoder.TokensEncoderFactory
 class LHRParser(override val model: LHRModel) : NeuralParser<LHRModel> {
 
   /**
-   * A more convenient access to the embeddings values of the virtual root.
+   * The Encoder of the Latent Syntactic Structure.
    */
-  private val virtualRoot: DenseNDArray get() = this.model.rootEmbedding.array.values
-
-  /**
-   * The builder of the tokens encoder.
-   */
-  private val tokensEncoderBuilder = TokensEncoderFactory(this.model.tokensEncoderModel, trainingMode = false)
-
-  /**
-   * The builder of the [ContextEncoder].
-   */
-  private val contextEncoderBuilder = ContextEncoderBuilder(this.model.contextEncoderModel)
-
-  /**
-   * The builder of the [HeadsEncoder].
-   */
-  private val headsEncoderBuilder = HeadsEncoderBuilder(this.model.headsEncoderModel)
+  private val lssEncoder = LSSEncoder(
+    tokensEncoder = TokensEncoderFactory(this.model.tokensEncoderModel, useDropout = false),
+    contextEncoder = ContextEncoder(this.model.contextEncoderModel, useDropout = false),
+    headsEncoder = HeadsEncoder(this.model.headsEncoderModel, useDropout = false),
+    virtualRoot = this.model.rootEmbedding.array.values)
 
   /**
    * The builder of the labeler.
    */
-  private val deprelLabelerBuilder: DeprelLabelerBuilder? = this.model.labelerModel?.let {
-    DeprelLabelerBuilder(model = it)
+  private val deprelLabeler: DeprelLabeler? = this.model.labelerModel?.let {
+    DeprelLabeler(it, useDropout = false)
   }
 
   /**
@@ -71,21 +57,15 @@ class LHRParser(override val model: LHRModel) : NeuralParser<LHRModel> {
    */
   override fun parse(sentence: Sentence): DependencyTree {
 
-    val encoder: LSSEncoder = this.buildEncoder()
-    val lss = encoder.encode(sentence.tokens)
+    val parsingSentence = ParsingSentence(tokens = (sentence.tokens.mapIndexed { index, token ->
+      ParsingToken(index, token.word, position = Position(0, 0, 0))
+    }))
+
+    val lss: LatentSyntacticStructure = this.lssEncoder.encode(parsingSentence)
     val scores: ArcScores = CosineDecoder().decode(lss)
 
     return this.buildDependencyTree(lss, scores)
   }
-
-  /**
-   * @return a LSSEncoder
-   */
-  private fun buildEncoder() = LSSEncoder(
-    tokensEncoder = this.tokensEncoderBuilder.invoke(),
-    contextEncoder = this.contextEncoderBuilder.invoke(),
-    headsEncoder = this.headsEncoderBuilder.invoke(),
-    virtualRoot = this.virtualRoot)
 
   /**
    * @param lss the latent syntactic structure
@@ -95,7 +75,7 @@ class LHRParser(override val model: LHRModel) : NeuralParser<LHRModel> {
    */
   private fun buildDependencyTree(lss: LatentSyntacticStructure, scores: ArcScores): DependencyTree {
 
-    val dependencyTree = DependencyTree(lss.tokens.size)
+    val dependencyTree = DependencyTree(lss.size)
 
     dependencyTree.let {
       this.assignHeads(it, scores)
@@ -110,7 +90,7 @@ class LHRParser(override val model: LHRModel) : NeuralParser<LHRModel> {
    * @param dependencyTree the dependency tree to annotate with the heads
    * @param scores the attachment scores
    */
-  private fun assignHeads(dependencyTree: DependencyTree, scores: ArcScores) {
+  fun assignHeads(dependencyTree: DependencyTree, scores: ArcScores) {
 
     val (topId: Int, topScore: Double) = scores.findHighestScoringTop()
 
@@ -142,9 +122,7 @@ class LHRParser(override val model: LHRModel) : NeuralParser<LHRModel> {
    */
   private fun assignLabels(dependencyTree: DependencyTree, lss: LatentSyntacticStructure) {
 
-    val labeler: DeprelLabeler? = this@LHRParser.deprelLabelerBuilder?.invoke()
-
-    labeler?.let {
+    this.deprelLabeler?.let {
       it.forward(DeprelLabeler.Input(lss, dependencyTree)).forEachIndexed { tokenId, prediction ->
         dependencyTree.setDeprel(tokenId, it.getDeprel(prediction.deprels.argMaxIndex()))
       }
