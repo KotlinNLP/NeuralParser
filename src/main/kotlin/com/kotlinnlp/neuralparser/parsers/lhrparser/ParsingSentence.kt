2558a7c57b546a98c299552986d48a7b25c62ae9
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-12 02:38:58 +0200

    Add ParsingSentence

---
+++ src/main/kotlin/com/kotlinnlp/neuralparser/parsers/lhrparser/ParsingSentence.kt
@@ -0,0 +1,15 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.neuralparser.parsers.lhrparser
+
+import com.kotlinnlp.linguisticdescription.sentence.Sentence
+
+/**
+ * @property tokens the tokens
+ */
+class ParsingSentence(override val tokens: List<ParsingToken>) : Sentence<ParsingToken>
