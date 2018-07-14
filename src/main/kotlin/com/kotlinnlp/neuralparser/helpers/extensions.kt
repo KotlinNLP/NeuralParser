1c3f6cc8ec166c34b331624462bf590db7b00cbe
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-14 18:36:11 +0200

    Add extensions

---
+++ src/main/kotlin/com/kotlinnlp/neuralparser/helpers/extensions.kt
@@ -0,0 +1,70 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.neuralparser.helpers
+
+import com.kotlinnlp.dependencytree.DependencyTree
+import com.kotlinnlp.dependencytree.Deprel
+import com.kotlinnlp.dependencytree.POSTag
+import com.kotlinnlp.linguisticdescription.sentence.token.properties.Position
+import com.kotlinnlp.neuralparser.language.ParsingSentence
+import com.kotlinnlp.neuralparser.language.ParsingToken
+import com.kotlinnlp.conllio.Sentence as CoNLLSentence
+import com.kotlinnlp.conllio.Token as CoNLLToken
+
+/**
+ * Transform this CoNLL sentence into a [ParsingSentence].
+ *
+ * @return a parsing sentence
+ */
+fun CoNLLSentence.toParsingSentence() = ParsingSentence(tokens = (this.tokens.map { token ->
+  ParsingToken(
+    id = token.id - 1,
+    form = token.form,
+    position = Position(index = token.id - 1, start = 0, end = 0),
+    posTag = token.pos)
+}))
+
+/**
+ * Return the [DependencyTree] of this sentence.
+ *
+ * @return the dependency tree
+ */
+internal fun CoNLLSentence.getDependencyTree(): DependencyTree {
+
+  val dependencyTree = DependencyTree(size = this.tokens.size)
+
+  this.tokens.forEach { token -> dependencyTree.addArc(token) }
+
+  return dependencyTree
+}
+
+/**
+ * Add the arc defined by the id, head and deprel of a given [token].
+ *
+ * @param token a token
+ */
+private fun DependencyTree.addArc(token: CoNLLToken) {
+
+  val id = token.id - 1
+  val head = if (token.head!! == 0) null else token.head!! - 1
+
+  val deprel = Deprel(
+    label = token.deprel,
+    direction = when {
+      head == null -> Deprel.Position.ROOT
+      head > id -> Deprel.Position.LEFT
+      else -> Deprel.Position.RIGHT
+    })
+
+  if (head != null) {
+    this.setArc(dependent = id, governor = head, deprel = deprel, posTag = POSTag(label = token.pos))
+  } else {
+    this.setDeprel(dependent = id, deprel = deprel)
+    this.setPosTag(dependent = id, posTag = POSTag(label = token.pos))
+  }
+}
