e40b433455743b84e4373178435a51005789d2ca
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-14 18:20:42 +0200

    Add toMorphoSyntacticSentence() method

--- src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingSentence.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingSentence.kt
@@ -7,9 +7,36 @@
 
 package com.kotlinnlp.neuralparser.language
 
+import com.kotlinnlp.dependencytree.DependencyTree
+import com.kotlinnlp.linguisticdescription.sentence.MorphoSyntacticSentence
 import com.kotlinnlp.linguisticdescription.sentence.Sentence
+import com.kotlinnlp.linguisticdescription.sentence.token.Word
+import com.kotlinnlp.linguisticdescription.sentence.token.properties.DependencyRelation
 
 /**
  * @property tokens the tokens
  */
-class ParsingSentence(override val tokens: List<ParsingToken>) : Sentence<ParsingToken>
+class ParsingSentence(override val tokens: List<ParsingToken>) : Sentence<ParsingToken> {
+
+  /**
+   * @param dependencyTree the dependency tree from which to extract the dependency relations
+   *
+   * @return a new [MorphoSyntacticSentence]
+   */
+  fun toMorphoSyntacticSentence(dependencyTree: DependencyTree) = MorphoSyntacticSentence(
+    id = 0,  // TODO
+    confidence = 0.0, // TODO
+    tokens = this.tokens.map {
+      Word(
+        id = it.id,
+        form = it.form,
+        position = it.position,
+        lexicalInterpretations = emptyList(), // TODO
+        dependencyRelation = DependencyRelation(
+          governor = dependencyTree.heads[it.id], // TODO: fix issue index/id
+          deprel = dependencyTree.deprels[it.id]?.label ?: "_", // TODO: fix issue index/id
+          attachmentScore = 0.0 // TODO
+        ))
+    }
+  )
+}
