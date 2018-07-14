9d3ff853c253f15db804f69d817cb717d29e0ed4
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-14 18:17:31 +0200

    Move ParsingSentence and ParsingToken into the 'language' package

--- src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingSentence.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingSentence.kt
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * ------------------------------------------------------------------*/
 
-package com.kotlinnlp.neuralparser.parsers.lhrparser
+package com.kotlinnlp.neuralparser.language
 
 import com.kotlinnlp.linguisticdescription.sentence.Sentence
 
