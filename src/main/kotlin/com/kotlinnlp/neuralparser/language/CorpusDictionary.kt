0ba00b89a7c307b3a9af902fc530a3bde8abe4be
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-18 19:33:57 +0200

    Add comment

--- src/main/kotlin/com/kotlinnlp/neuralparser/language/CorpusDictionary.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/language/CorpusDictionary.kt
@@ -99,7 +99,7 @@ class CorpusDictionary : Serializable {
       label = token.deprel,
       direction = when {
         head == 0 -> Deprel.Position.ROOT
-        head > token.id -> Deprel.Position.LEFT
+        head > token.id -> Deprel.Position.LEFT // the id follows the incremental order of the CoNLL sentence tokens
         else -> Deprel.Position.RIGHT
       })
   }
