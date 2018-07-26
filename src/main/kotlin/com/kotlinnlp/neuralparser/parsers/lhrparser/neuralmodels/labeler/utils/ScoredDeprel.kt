3499dcc8f8c6e8648112523e6c09e0bd05aa8733
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-26 13:18:26 +0200

    Add ScoredDeprel

---
+++ src/main/kotlin/com/kotlinnlp/neuralparser/parsers/lhrparser/neuralmodels/labeler/utils/ScoredDeprel.kt
@@ -0,0 +1,38 @@
+/* Copyright 2018-present LHRParser Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * -----------------------------------------------------------------------------*/
+
+package com.kotlinnlp.neuralparser.parsers.lhrparser.neuralmodels.labeler.utils
+
+import com.kotlinnlp.dependencytree.Deprel
+
+/**
+ * The outcome of a single prediction of the labeler.
+ *
+ * @property value the deprel
+ * @property score the score
+ */
+data class ScoredDeprel(val value: Deprel, val score: Double) {
+
+  companion object {
+
+    /**
+     * A ScoredDeprel [Comparator] by score.
+     */
+    val deprelComparator = compareByDescending<ScoredDeprel> { it.score }
+  }
+}
+
+/**
+ * List of ScoredDeprel,
+ */
+typealias ScoredDeprelList = List<ScoredDeprel>
+
+/**
+ * @return a sorted the list of prediction by descending score
+ */
+fun ScoredDeprelList.sortByScore() = this.sortedWith(ScoredDeprel.deprelComparator)
+
