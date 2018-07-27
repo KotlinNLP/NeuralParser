a3be966dcac4475142295c358e4f49325d18ccde
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-27 13:15:14 +0200

    Add header

--- src/main/kotlin/com/kotlinnlp/neuralparser/helpers/preprocessors/BasePreprocessor.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/helpers/preprocessors/BasePreprocessor.kt
@@ -1,3 +1,10 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
 package com.kotlinnlp.neuralparser.helpers.preprocessors
 
 import com.kotlinnlp.neuralparser.language.BaseSentence
