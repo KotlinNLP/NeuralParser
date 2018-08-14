8f3e096e32c69a951550274046e041cb73c3779e
KotlinNLP Authors <github@kotlinnlp.com>
2018-08-14 11:35:10 +0200

    Make the 'position' property private and update the toMorphoSyntacticToken() method to build WordTraces too

--- src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingToken.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingToken.kt
@@ -9,10 +9,7 @@ package com.kotlinnlp.neuralparser.language
 
 import com.kotlinnlp.linguisticdescription.morphology.Morphology
 import com.kotlinnlp.linguisticdescription.morphology.ScoredMorphology
-import com.kotlinnlp.linguisticdescription.sentence.token.MorphoSyntacticToken
-import com.kotlinnlp.linguisticdescription.sentence.token.MorphoToken
-import com.kotlinnlp.linguisticdescription.sentence.token.RealToken
-import com.kotlinnlp.linguisticdescription.sentence.token.Word
+import com.kotlinnlp.linguisticdescription.sentence.token.*
 import com.kotlinnlp.linguisticdescription.sentence.token.properties.DependencyRelation
 import com.kotlinnlp.linguisticdescription.sentence.token.properties.Position
 import com.kotlinnlp.neuralparser.parsers.lhrparser.deprelselectors.MorphoDeprelSelector
@@ -20,19 +17,19 @@ import com.kotlinnlp.neuralparser.parsers.lhrparser.deprelselectors.MorphoDeprel
 /**
  * The token of the [ParsingSentence].
  *
- * @param id the token id, an incremental integer starting from 0 within a sentence
- * @param form the form
- * @param position the position of the token in the text
- * @param morphologies the list of possible morphologies of the token
- * @param posTag the best part-of-speech tags associated to the token (can be null) TODO: find a better solution
+ * @property id the token id, an incremental integer starting from 0 within a sentence
+ * @property form the form
+ * @property morphologies the list of possible morphologies of the token
+ * @property posTag the best part-of-speech tags associated to the token (can be null)
+ * @param position the position of the token in the text (null if it is a trace)
  */
 data class ParsingToken(
   val id: Int,
   override val form: String,
-  override val position: Position,
   override val morphologies: List<Morphology>,
-  val posTag: String? = null
-) : MorphoToken, RealToken {
+  val posTag: String? = null, // TODO: find a better solution
+  private val position: Position?
+) : MorphoToken, FormToken {
 
   /**
    * @param dependencyRelation the dependency relation of this token
@@ -41,18 +38,30 @@ data class ParsingToken(
    * @return a new morpho syntactic token
    */
   fun toMorphoSyntacticToken(dependencyRelation: DependencyRelation,
-                             morphoDeprelSelector: MorphoDeprelSelector): MorphoSyntacticToken =
-    Word(
-      id = this.id,
-      form = this.form,
-      position = this.position,
-      morphologies = morphoDeprelSelector.getValidMorphologies(
-        token = this,
-        morphologies = this.morphologies,
-        deprelLabel = dependencyRelation.deprel
-      ).map { ScoredMorphology(type = it.type, list = it.list, score = 0.0) }, // TODO: set the score?
-      dependencyRelation = dependencyRelation,
-      coReferences = null, // TODO: set it
-      semanticRelations = null // TODO: set it
-    )
+                             morphoDeprelSelector: MorphoDeprelSelector): MorphoSyntacticToken {
+
+    val morphologies: List<ScoredMorphology> = morphoDeprelSelector.getValidMorphologies(
+      token = this,
+      morphologies = this.morphologies,
+      deprelLabel = dependencyRelation.deprel
+    ).map { ScoredMorphology(type = it.type, list = it.list, score = 0.0) } // TODO: set the score?
+
+    return if (this.position != null)
+      Word(
+        id = this.id,
+        form = this.form,
+        position = this.position,
+        morphologies = morphologies,
+        dependencyRelation = dependencyRelation,
+        coReferences = null, // TODO: set it
+        semanticRelations = null) // TODO: set it
+    else
+      WordTrace(
+        id = this.id,
+        form = this.form,
+        morphologies = morphologies,
+        dependencyRelation = dependencyRelation,
+        coReferences = null, // TODO: set it
+        semanticRelations = null)
+  }
 }
