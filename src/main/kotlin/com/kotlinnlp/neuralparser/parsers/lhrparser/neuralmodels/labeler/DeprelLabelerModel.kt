f3f7a2e7717a8c03e118855d3d1e24c9b2cd5937
KotlinNLP Authors <github@kotlinnlp.com>
2018-08-22 12:08:50 +0200

    Add 'serialVersionUID'

--- src/main/kotlin/com/kotlinnlp/neuralparser/parsers/lhrparser/neuralmodels/labeler/DeprelLabelerModel.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/parsers/lhrparser/neuralmodels/labeler/DeprelLabelerModel.kt
@@ -33,6 +33,15 @@ class DeprelLabelerModel(
   val lossCriterionType: LossCriterionType
 ) : Serializable {
 
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed by Serializable).
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+  }
+
   /**
    * The Network model that predicts the Deprels
    */
