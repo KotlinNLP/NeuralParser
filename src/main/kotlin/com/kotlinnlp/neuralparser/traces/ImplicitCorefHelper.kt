9048b438091b45d9a745caa76db567721eee383d
KotlinNLP Authors <github@kotlinnlp.com>
2018-08-27 11:30:46 +0200

    Add co-reference helper for implicit subordinates

---
+++ src/main/kotlin/com/kotlinnlp/neuralparser/traces/ImplicitCorefHelper.kt
@@ -0,0 +1,43 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.neuralparser.traces
+
+import com.kotlinnlp.linguisticdescription.sentence.MorphoSyntacticSentence
+import com.kotlinnlp.linguisticdescription.sentence.token.properties.CoReference
+
+/**
+ *
+ */
+class ImplicitCorefHelper : CorefHelper {
+
+  /**
+   *
+   */
+  override fun setCoref(sentence: MorphoSyntacticSentence) {
+
+    sentence.tokens.filter {
+      it.isVerb && it.isRMod && (it.isVerbGerundive || it.isVerbParticiple)
+    }.forEach { token ->
+
+      sentence.getTraceSubj(token)?.let { subj ->
+        sentence.getGovernor(token)?.let { governor ->
+
+          if (governor.isVerb) {
+
+            sentence.getSubj(governor)?.let {
+              subj.addCoReference(CoReference(
+                tokenId = it.id,
+                sentenceId = sentence.id,
+                confidence = 0.0))
+            }
+          }
+        }
+      }
+    }
+  }
+}
