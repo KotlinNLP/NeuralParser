f189d328f51a6cdcf0fcabda647ce5d7a7832c72
KotlinNLP Authors <github@kotlinnlp.com>
2018-09-27 15:30:11 +0200

    Update the creation of the components of a WordComposite in terms of dependencies

--- src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingToken.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingToken.kt
@@ -9,13 +9,16 @@ package com.kotlinnlp.neuralparser.language
 
 import com.kotlinnlp.linguisticdescription.GrammaticalConfiguration
 import com.kotlinnlp.linguisticdescription.POSTag
-import com.kotlinnlp.linguisticdescription.morphology.Morphology
-import com.kotlinnlp.linguisticdescription.morphology.ScoredMorphology
-import com.kotlinnlp.linguisticdescription.morphology.ScoredSingleMorphology
+import com.kotlinnlp.linguisticdescription.morphology.*
+import com.kotlinnlp.linguisticdescription.morphology.morphologies.relations.Preposition
+import com.kotlinnlp.linguisticdescription.morphology.morphologies.relations.Verb
+import com.kotlinnlp.linguisticdescription.morphology.morphologies.things.Article
+import com.kotlinnlp.linguisticdescription.morphology.morphologies.things.Pronoun
 import com.kotlinnlp.linguisticdescription.sentence.token.*
 import com.kotlinnlp.linguisticdescription.sentence.token.properties.SyntacticRelation
 import com.kotlinnlp.linguisticdescription.sentence.token.properties.Position
 import com.kotlinnlp.neuralparser.parsers.lhrparser.neuralmodules.labeler.selector.LabelerSelector
+import kotlin.reflect.KClass
 
 /**
  * The token of the [ParsingSentence].
@@ -35,6 +38,9 @@ data class ParsingToken(
 ) : MorphoToken, FormToken, TokenIdentificable {
 
   /**
+   * @param sentence the sentence this token is part of
+   * @param tokenIndex the index of this token within the sentence tokens list
+   * @param nextIdAvailable the next id that can be assigned to a new token to add to the sentence (as component)
    * @param governorId the governor id
    * @param attachmentScore the attachment score
    * @param config the grammatical configuration of this token
@@ -44,6 +50,7 @@ data class ParsingToken(
    */
   fun toMorphoSynToken(sentence: ParsingSentence,
                        tokenIndex: Int,
+                       nextIdAvailable: Int,
                        governorId: Int?,
                        attachmentScore: Double,
                        config: GrammaticalConfiguration,
@@ -62,27 +69,22 @@ data class ParsingToken(
 
     return if (config.components.size == 1)
       this.buildSingleToken(
+        id = this.id,
         governorId = governorId,
         attachmentScore = attachmentScore,
         grammaticalComponent = config.components.single(),
         morphologies = morphologies.map { it.toSingle() })
     else
-      MorphoSynToken.Composite(
-        id = this.id,
-        form = this.form,
-        position = checkNotNull(this.position) { "Composite words must have a position." },
-        components = config.components.mapIndexed { i, component ->
-          this.buildSingleToken(
-            governorId = if (i == 0) governorId else null,
-            attachmentScore = attachmentScore,
-            grammaticalComponent = component,
-            morphologies = morphologies.map { ScoredSingleMorphology(value = it.components[i], score = it.score) }
-          ) as Word
-        }
-      )
+      this.buildCompositeToken(
+        nextIdAvailable = nextIdAvailable,
+        governorId = governorId,
+        attachmentScore = attachmentScore,
+        config = config,
+        morphologies = morphologies)
   }
 
   /**
+   * @param id the id of the new token
    * @param governorId the governor id
    * @param attachmentScore the attachment score
    * @param grammaticalComponent the grammatical configuration of the token as single component
@@ -90,10 +92,11 @@ data class ParsingToken(
    *
    * @return a new single token
    */
-  private fun buildSingleToken(governorId: Int?,
+  private fun buildSingleToken(id: Int,
+                               governorId: Int?,
                                attachmentScore: Double,
                                grammaticalComponent: GrammaticalConfiguration.Component,
-                               morphologies: List<ScoredSingleMorphology>): MorphoSynToken {
+                               morphologies: List<ScoredSingleMorphology>): MorphoSynToken.Single {
 
     val syntacticRelation = SyntacticRelation(
       governor = governorId,
@@ -102,7 +105,7 @@ data class ParsingToken(
 
     return if (this.position != null)
       Word(
-        id = this.id,
+        id = id,
         form = this.form,
         position = this.position,
         pos = grammaticalComponent.pos,
@@ -112,7 +115,7 @@ data class ParsingToken(
         semanticRelations = null) // TODO: set it
     else
       WordTrace(
-        id = this.id,
+        id = id,
         form = this.form,
         pos = grammaticalComponent.pos,
         morphologies = morphologies,
@@ -120,4 +123,66 @@ data class ParsingToken(
         coReferences = null, // TODO: set it
         semanticRelations = null)
   }
+
+  /**
+   * @param nextIdAvailable the next id that can be assigned to a new token to add to the sentence (as component)
+   * @param governorId the governor id
+   * @param attachmentScore the attachment score
+   * @param config the grammatical configuration of the token
+   * @param morphologies the list of possible scored morphologies of the token
+   *
+   * @return a new composite token
+   */
+  private fun buildCompositeToken(nextIdAvailable: Int,
+                                  governorId: Int?,
+                                  attachmentScore: Double,
+                                  config: GrammaticalConfiguration,
+                                  morphologies: List<ScoredMorphology>): MorphoSynToken.Composite {
+
+    val isPrepArt: Boolean = config.isPrepArt()
+    val isVerbEnclitic: Boolean = config.isVerbEnclitic()
+
+    return MorphoSynToken.Composite(
+      id = this.id,
+      form = this.form,
+      position = checkNotNull(this.position) { "Composite words must have a position." },
+      components = config.components.mapIndexed { i, component ->
+        this.buildSingleToken(
+          id = nextIdAvailable + i,
+          governorId = when {
+            i == 0 || isPrepArt -> governorId
+            isVerbEnclitic -> nextIdAvailable // the ID of the first component
+            else -> null
+          },
+          attachmentScore = attachmentScore,
+          grammaticalComponent = component,
+          morphologies = morphologies.map { ScoredSingleMorphology(value = it.components[i], score = it.score) }
+        ) as Word
+      }
+    )
+  }
+
+  /**
+   * @return true if this configuration defines a composite PREP + ART, otherwise false
+   */
+  private fun GrammaticalConfiguration.isPrepArt(): Boolean =
+    this.components.size == 2 &&
+      this.components[0].isSubTypeOf(Preposition::class) &&
+      this.components[1].isSubTypeOf(Article::class)
+
+  /**
+   * @return true if this configuration defines a composite VERB + PRON, otherwise false
+   */
+  private fun GrammaticalConfiguration.isVerbEnclitic(): Boolean =
+    this.components.size >= 2 &&
+      this.components[0].isSubTypeOf(Verb::class) &&
+      this.components.subList(1, this.components.size).all { it.isSubTypeOf(Pronoun::class) }
+
+  /**
+   * @param morphologyClass the KClass of a single morphology
+   *
+   * @return true if this component defines a POS that is a subtype of the type of given morphology, otherwise false
+   */
+  private fun <T : SingleMorphology>GrammaticalConfiguration.Component.isSubTypeOf(morphologyClass: KClass<T>): Boolean
+    = this.pos.let { it is POSTag.Base && it.type.isSubTypeOf(morphologyClass) }
 }
