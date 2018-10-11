8b6c39129aee63a6f01bfd889f63f725c948134c
KotlinNLP Authors <github@kotlinnlp.com>
2018-10-11 18:07:18 +0200

    Make method internal

--- src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingToken.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingToken.kt
@@ -27,14 +27,14 @@ import kotlin.reflect.KClass
  * @property form the form
  * @property morphologies the list of possible morphologies of the token
  * @property pos the list of part-of-speech tags associated to the token (more for composite tokens, can be null)
- * @param position the position of the token in the text (null if it is a trace)
+ * @property position the position of the token in the text (null if it is a trace)
  */
 data class ParsingToken(
   override val id: Int,
   override val form: String,
   override val morphologies: List<Morphology>,
   override val pos: List<POSTag>? = null, // TODO: find a better solution
-  private val position: Position?
+  val position: Position?
 ) : MorphoToken, FormToken, TokenIdentificable {
 
   /**
@@ -48,13 +48,13 @@ data class ParsingToken(
    *
    * @return a new morpho syntactic token
    */
-  fun toMorphoSynToken(sentence: ParsingSentence,
-                       tokenIndex: Int,
-                       nextIdAvailable: Int,
-                       governorId: Int?,
-                       attachmentScore: Double,
-                       config: GrammaticalConfiguration,
-                       labelerSelector: LabelerSelector): MorphoSynToken {
+  internal fun toMorphoSynToken(sentence: ParsingSentence,
+                                tokenIndex: Int,
+                                nextIdAvailable: Int,
+                                governorId: Int?,
+                                attachmentScore: Double,
+                                config: GrammaticalConfiguration,
+                                labelerSelector: LabelerSelector): MorphoSynToken {
 
     // TODO: set the score adding the labeler prediction scores of configurations with the same pos
     val morphologies: List<ScoredMorphology> = labelerSelector.getValidMorphologies(
