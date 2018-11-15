990168fa6c4ebfe6f74325c5a210740b772d1212
KotlinNLP Authors <github@kotlinnlp.com>
2018-11-15 21:29:25 +0100

    Update SentenceValidator removing the BeamManager implementation

--- examples/constraints/utils/SentenceValidator.kt
+++ examples/constraints/utils/SentenceValidator.kt
@@ -7,12 +7,11 @@
 
 package constraints.utils
 
-import com.kotlinnlp.neuralparser.constraints.Constraint
-import com.kotlinnlp.dependencytree.DependencyTree
 import com.kotlinnlp.linguisticdescription.morphology.ScoredSingleMorphology
+import com.kotlinnlp.neuralparser.constraints.Constraint
 import com.kotlinnlp.linguisticdescription.sentence.token.MorphoSynToken
 import com.kotlinnlp.neuralparser.morphopercolator.MorphoPercolator
-import com.kotlinnlp.utils.BeamManager
+import com.kotlinnlp.utils.notEmptyOr
 
 /**
  * A helper that validate constraints on all the morphologies combinations of a list of tokens.
@@ -22,125 +21,32 @@ import com.kotlinnlp.utils.BeamManager
  * @param morphoPercolator a percolator of morphology properties
  */
 internal class SentenceValidator(
-  private val tokens: List<MorphoSynToken.Single>,
+  tokens: List<MorphoSynToken.Single>,
   private val constraints: List<Constraint>,
   private val morphoPercolator: MorphoPercolator
-) : BeamManager<SentenceValidator.MorphologyValue, SentenceValidator.MorphoState>(
-  valuesMap = tokens.associate { token ->
-    token.id to token.morphologies.asSequence().map { MorphologyValue(it) }.sortedByDescending { it.score }.toList()
-  },
-  maxBeamSize = -1,
-  maxForkSize = -1,
-  maxIterations = -1
-) {
+) : ConstraintsValidator(tokens) {
 
   /**
-   * A value containing a scored morphology.
-   *
-   * @property morphology a scored morphology of a token
+   * The input tokens associated to their list index.
    */
-  internal data class MorphologyValue(val morphology: ScoredSingleMorphology) : Value() {
-
-    /**
-     * The score of this value.
-     */
-    override var score: Double = this.morphology.score
-  }
+  private val indexedTokens: Iterable<IndexedValue<MorphoSynToken.Single>> = tokens.withIndex()
 
   /**
-   * The state that represents a configuration of the morphologies of the tokens.
-   *
-   * @param elements the list of elements in this state, sorted by diff score
+   * A list of constraints that do not check any morphology.
    */
-  internal inner class MorphoState(elements: List<StateElement<MorphologyValue>>) : State(elements) {
-
-    /**
-     * The global score of the state.
-     */
-    override val score: Double = 1.0
-
-    /**
-     * Whether this state does not violate any hard constraint.
-     */
-    override var isValid: Boolean = true
-
-    /**
-     * The map of token ids to the list of constraints that violate in this state.
-     */
-    val violatedConstraints: MutableMap<Int, MutableList<Constraint>> = mutableMapOf()
-
-    /**
-     * Apply the linguistic constraints to this state and set its score.
-     */
-    init {
-
-      // Attention: do not modify the order of the operations.
-      this.applyMorphologies()
-      this.applyConstraints(simpleConstraints)
-      morphoPercolator.applyPercolation(tokens = tokens, dependencyTree = dependencyTree).forEach { contextConfig ->
-        this.applyContextMorphologies(contextConfig)
-        this.applyConstraints(contextConstraints)
-      }
-    }
-
-    /**
-     * Apply the morphologies of this state to the tokens.
-     */
-    private fun applyMorphologies() {
-
-      this.elements.forEach { element ->
-
-        val token: MorphoSynToken.Single = tokens[dependencyTree.getPosition(element.id)]
-
-        token.removeAllMorphologies()
-        token.addMorphology(element.value.morphology)
-      }
-    }
-
-    /**
-     * Apply the given context morphologies to the tokens.
-     *
-     * @param morphologiesPerToken a map of context morphologies associated by token id
-     */
-    private fun applyContextMorphologies(morphologiesPerToken: Map<Int, ScoredSingleMorphology>) {
-
-      tokens.forEach { token ->
-        token.removeAllContextMorphologies()
-        token.addContextMorphology(morphologiesPerToken.getValue(token.id))
-      }
-    }
-
-    /**
-     * Apply the given [constraints] to this state.
-     *
-     * @param constraints a list of constraints to apply to the [tokens]
-     */
-    private fun applyConstraints(constraints: List<Constraint>) {
-
-      constraints.forEach { constraint ->
-        tokens.forEachIndexed { i, token ->
-
-          val isVerified: Boolean =
-            constraint.isVerified(token = token, tokens = tokens, dependencyTree = dependencyTree)
-
-          if (!isVerified && constraint.isHard) {
-            this.violatedConstraints.getOrPut(i) { mutableListOf() }.add(constraint)
-            this.isValid = false
-          }
-        }
-      }
-    }
-  }
+  private val simpleConstraints: List<Constraint> = this.constraints.filter { !it.checkMorpho }
 
   /**
-   * The dependency tree that represents the dependencies of the tokens.
+   * A list of constraints that check the base morphology of a single token.
    */
-  private val dependencyTree = DependencyTree(this.tokens)
+  private val morphoUnaryConstraints: List<Constraint> =
+    this.constraints.filter { it.checkMorpho && !it.checkContext && it.isUnary }
 
   /**
-   * A list of constraints that do not check the context morphology.
+   * A list of constraints that check the base morphology.
    */
-  private val simpleConstraints: List<Constraint> = this.constraints.filter { !it.checkContext }
+  private val morphoConstraints: List<Constraint> =
+    this.constraints.filter { it.checkMorpho && !it.checkContext && !it.isUnary }
 
   /**
    * A list of constraints that check the context morphology.
@@ -148,31 +54,108 @@ internal class SentenceValidator(
   private val contextConstraints: List<Constraint> = this.constraints.filter { it.checkContext }
 
   /**
+   * All the possible morphologies for each token.
+   */
+  private val tokensMorphologies: List<List<ScoredSingleMorphology>> = this.tokens.map { it.morphologies }
+
+  /**
+   * A validator of the morphological configuration of a sentence.
+   */
+  private val morphoValidator = MorphoValidator(
+    tokens = this.tokens,
+    morphoConstraints = this.morphoConstraints,
+    contextConstraints = this.contextConstraints,
+    morphoPercolator = this.morphoPercolator)
+
+  /**
    * Find all the constraints violated by each token.
    *
-   * @return a map of tokens to the list of constraints that they violate
+   * @return the map of all the linguistic constraints violated by the input sentence
    */
-  fun validate(): Map<MorphoSynToken.Single, Set<Constraint>> {
+  fun validate(): ViolationsMap {
 
-    val states: List<MorphoState> = this.findConfigurations(onlyValid = false)
-    val consideredStates: List<MorphoState> = if (states.first().isValid) listOf(states.first()) else states
+    val violationsMap: ViolationsMap = this.verifyConstraints(this.simpleConstraints).notEmptyOr {
+      this.getUnaryMorphoViolations().notEmptyOr {
+        this.getMorphoViolations()
+      }
+    }
 
-    return consideredStates.fold(mutableMapOf()) { violationsMap, state ->
+    return violationsMap.mapValues { it.value.asSequence().toSet().toList() }
+  }
 
-      state.violatedConstraints.forEach { tokenIndex, constraint ->
-        violationsMap.merge(this.tokens[tokenIndex], constraint.toSet()) { t, u -> t + u }
+  /**
+   * @return the map of all the unary linguistic constraints violated by the input sentence
+   */
+  private fun getUnaryMorphoViolations(): ViolationsMap =
+    this.tokens.asSequence().collectViolations { this.getUnaryMorphoViolations(it) }
+
+  /**
+   * @param token an input token
+   *
+   * @return the map of all the unary linguistic constraints violated by the given token
+   */
+  private fun getUnaryMorphoViolations(token: MorphoSynToken.Single): ViolationsMap {
+
+    val morphologies: List<ScoredSingleMorphology> = token.morphologies
+
+    val violations: ViolationsMap = morphologies.asSequence().collectViolations { morphology ->
+
+      token.removeAllMorphologies()
+      token.addMorphology(morphology) // set one morphology per iteration
+
+      val violations: List<Constraint> = this.morphoUnaryConstraints.filter {
+        !it.isVerified(token = token, tokens = this.tokens, dependencyTree = this.dependencyTree)
       }
 
-      violationsMap
+      if (violations.isNotEmpty()) mapOf(token.id to violations) else mapOf()
+    }
+
+    token.removeAllMorphologies()
+    morphologies.forEach { token.addMorphology(it) } // restore all morphologies
+
+    return violations
+  }
+
+  /**
+   * @return the map of all the morphological linguistic constraints violated by the input sentence
+   */
+  private fun getMorphoViolations(): ViolationsMap =
+    this.getAllMorphoConfigurations().collectViolations { this.morphoValidator.getViolations(it) }
+
+  /**
+   * @param focusIndex the index of the focus token within the recursion
+   * @param morphoIndices the indices of the focus morphologies, one per token
+   *
+   * @return all the possible morphological configurations of the input [tokens]
+   */
+  private fun getAllMorphoConfigurations(
+    focusIndex: Int = 0,
+    morphoIndices: MutableList<Int> = MutableList(size = this.tokens.size, init = { 0 })
+  ): Sequence<MorphoConfiguration> = sequence {
+
+    tokensMorphologies[focusIndex].indices.forEach { focusMorphoIndex ->
+
+      morphoIndices[focusIndex] = focusMorphoIndex
+
+      yield(buildMorphoConfiguration(morphoIndices))
+
+      if (focusIndex < tokens.lastIndex)
+        yieldAll(getAllMorphoConfigurations(focusIndex = focusIndex + 1, morphoIndices = morphoIndices))
     }
   }
 
   /**
-   * Build a new state with the given elements.
+   * @param morphoIndices the indices of the focus morphologies, one per token
    *
-   * @param elements the elements that compose the building state
-   *
-   * @return a new state with the given elements
+   * @return a morphological configuration of the input sentence built using the token morphologies at the given indices
    */
-  override fun buildState(elements: List<StateElement<MorphologyValue>>): MorphoState = MorphoState(elements)
+  private fun buildMorphoConfiguration(morphoIndices: List<Int>): MorphoConfiguration =
+
+    this.indexedTokens.associate { (tokenIndex, token) ->
+
+      val tokenMorphologies: List<ScoredSingleMorphology> = this.tokensMorphologies[tokenIndex]
+      val tokenMorphoIndex: Int = morphoIndices[tokenIndex]
+
+      token.id to tokenMorphologies[tokenMorphoIndex]
+    }
 }
