0cef06946192da42e1525d1f2ffa4743bdef984f
KotlinNLP Authors <github@kotlinnlp.com>
2018-11-25 02:23:32 +0100

    Remove the implementation of the MorphoToken interface

--- src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingToken.kt
+++ src/main/kotlin/com/kotlinnlp/neuralparser/language/ParsingToken.kt
@@ -8,7 +8,6 @@
 package com.kotlinnlp.neuralparser.language
 
 import com.kotlinnlp.linguisticdescription.POSTag
-import com.kotlinnlp.linguisticdescription.morphology.*
 import com.kotlinnlp.linguisticdescription.sentence.token.*
 import com.kotlinnlp.linguisticdescription.sentence.token.properties.Position
 
@@ -17,14 +16,12 @@ import com.kotlinnlp.linguisticdescription.sentence.token.properties.Position
  *
  * @property id the id of the token, unique within its sentence
  * @property form the form
- * @property morphologies the list of possible morphologies of the token
  * @property pos the list of part-of-speech tags associated to the token (more for composite tokens, can be null)
  * @property position the position of the token in the text (null if it is a trace)
  */
 data class ParsingToken(
   override val id: Int,
   override val form: String,
-  override val morphologies: List<Morphology>,
-  override val pos: List<POSTag>? = null, // TODO: find a better solution
+  val pos: List<POSTag>? = null, // TODO: find a better solution
   val position: Position?
-) : MorphoToken, FormToken, TokenIdentificable
+) : FormToken, TokenIdentificable
