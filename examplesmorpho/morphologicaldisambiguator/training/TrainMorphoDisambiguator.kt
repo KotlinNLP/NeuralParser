3114acf1c54906dd4cb4479064c296667b8d16ed
KotlinNLP Authors <github@kotlinnlp.com>
2019-02-14 06:19:17 +0100

    First commit

---
+++ examplesmorpho/morphologicaldisambiguator/training/TrainMorphoDisambiguator.kt
@@ -0,0 +1,36 @@
+/* Copyright 2018-present KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * -----------------------------------------------------------------------------*/
+
+package morphologicaldisambiguator.training
+
+import com.xenomachina.argparser.mainBody
+import com.kotlinnlp.lssencoder.LSSModel
+import com.kotlinnlp.morphodisambiguator.helpers.dataset.Dataset
+import com.kotlinnlp.neuralparser.language.ParsingSentence
+import com.kotlinnlp.neuralparser.language.ParsingToken
+import com.kotlinnlp.neuralparser.parsers.lhrparser.LHRModel
+import java.io.File
+import java.io.FileInputStream
+
+fun main(args: Array<String>) = mainBody {
+
+  val parsedArgs = CommandLineArguments(args)
+
+  println("Loading training dataset from '${parsedArgs.trainingSetPath}'...")
+
+  val trainingDataset: Dataset = Dataset.fromFile(
+      filename = parsedArgs.trainingSetPath
+  )
+
+  val lssModel: LSSModel<ParsingToken, ParsingSentence> = parsedArgs.lssModelPath.let {
+    println("Loading the LSSEncoder model from the LHRParser model serialized in '$it'...")
+    LHRModel.load(FileInputStream(File(it))).lssModel
+  }
+
+  println("\n-- START TRAINING ON %d SENTENCES".format(trainingDataset.examples.size))
+
+}
