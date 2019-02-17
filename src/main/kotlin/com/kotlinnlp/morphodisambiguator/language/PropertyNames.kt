13752146c5d4c74dbc1896477d7700b6768c94f2
KotlinNLP Authors <github@kotlinnlp.com>
2019-02-17 17:00:03 +0100

    Initial commit

---
+++ src/main/kotlin/com/kotlinnlp/morphodisambiguator/language/PropertyNames.kt
@@ -0,0 +1,6 @@
+package com.kotlinnlp.morphodisambiguator.language
+
+/**
+ * The list of names of all the possible properties.
+ */
+data class PropertyNames(val propertyNames: List<String> = listOf("gender", "number", "person", "mood", "tense"))
