6f9b1e4307c4c3fe48f89cc47c286cd81b2adb72
KotlinNLP Authors <github@kotlinnlp.com>
2019-02-19 14:51:11 +0100

    Add Serializable interface

--- src/main/kotlin/com/kotlinnlp/morphodisambiguator/language/PropertyNames.kt
+++ src/main/kotlin/com/kotlinnlp/morphodisambiguator/language/PropertyNames.kt
@@ -1,6 +1,23 @@
 package com.kotlinnlp.morphodisambiguator.language
 
+import java.io.Serializable
+
 /**
  * The list of names of all the possible properties.
  */
-data class PropertyNames(val propertyNames: List<String> = listOf("gender", "number", "person", "mood", "tense"))
+data class PropertyNames(
+    val propertyNames: List<String> = listOf("gender", "number", "person", "mood", "tense")
+): Serializable {
+  companion object {
+
+
+    /**
+     * Private val used to serialize the class (needed by Serializable).
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+
+  }
+
+
+}
